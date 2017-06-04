92ab3f1d35dfdc69fe2367e7852b994dbfb2a497
Mati Halperin <matias.halperin@gmail.com>
2017-06-04 20:15:29 -0300

    SetupWizard: rebrand to CypherOS

--- exit_wizard.sh
+++ exit_wizard.sh
@@ -2,12 +2,12 @@
 
 adb root
 wait ${!}
-adb shell pm enable com.cyanogenmod.setupwizard/com.cyanogenmod.setupwizard.SetupWizardExitActivity || true
+adb shell pm enable com.aoscp.setupwizard/com.aoscp.setupwizard.SetupWizardExitActivity || true
 wait ${!}
 adb shell pm enable com.google.android.setupwizard/com.google.android.setupwizard.SetupWizardExitActivity || true
 wait ${!}
 sleep 1
-adb shell am start com.cyanogenmod.setupwizard/com.cyanogenmod.setupwizard.SetupWizardExitActivity || true
+adb shell am start com.aoscp.setupwizard/com.aoscp.setupwizard.SetupWizardExitActivity || true
 wait ${!}
 sleep 1
 adb shell am start com.google.android.setupwizard/com.google.android.setupwizard.SetupWizardExitActivity
