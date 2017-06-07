8da2837d0f3ac4342d9defa0d84056786c559130
Chris <chris@cypheros.co>
2017-06-06 22:12:00 -0400

    suw: Import common aoscp infrastructure
    Initial bring up of the aoscp setupwizard.
    Dumps all of lineage's common dependencies,
    such as permissions and action name scheming.
    
    Squash of:
    https://gerrit.cypheros.co/#/c/253/
    https://gerrit.cypheros.co/#/c/254/
    https://gerrit.cypheros.co/#/c/255/
    
    Signed-off by: Mati Halperin <matias.halperin@gmail.com>
    Signed-off by: Chris <chris@cypheros.co>
    
    Change-Id: Ib06b33e1466a1556ef95007b0bdb39275d69e322

--- start_aoscp_wizard.sh
+++ start_aoscp_wizard.sh
@@ -6,4 +6,4 @@ adb shell pm disable com.google.android.setupwizard || true
 wait ${!}
 adb shell pm disable com.android.provision || true
 wait ${!}
-adb shell am start com.aoscp.setupwizard/com.aoscp.setupwizard.SetupWizardTestActivity
+adb shell am start org.aoscp.setupwizard/org.aoscp.setupwizard.SetupWizardTestActivity
