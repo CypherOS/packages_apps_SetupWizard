c4e58d93df26bc23273ece1994984c32b69b8aab
Chris <chris@cypheros.co>
2017-06-07 03:35:54 -0400

    suw: Implement new design policy integration
    Change-Id: I369588b74b092dbb4e11ad562e33100c8f837de7

--- src/org/aoscp/setupwizard/WelcomeActivity.java
+++ src/org/aoscp/setupwizard/WelcomeActivity.java
@@ -20,6 +20,8 @@ package org.aoscp.setupwizard;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import org.aoscp.setupwizard.util.EnableAccessibilityController;
 
@@ -27,6 +29,7 @@ public class WelcomeActivity extends BaseSetupWizardActivity {
 
     public static final String TAG = WelcomeActivity.class.getSimpleName();
 
+	private Button mButton;
     private View mRootView;
     private EnableAccessibilityController mEnableAccessibilityController;
 
@@ -34,6 +37,13 @@ public class WelcomeActivity extends BaseSetupWizardActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mRootView = findViewById(R.id.root);
+		mButton = (Button) findViewById(R.id.start);
+		mButton.setText(R.string.start);
+		mButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onNavigateNext();
+            }
+        });
         setNextText(R.string.next);
         setBackText(R.string.emergency_call);
         setBackDrawable(null);
@@ -55,6 +65,11 @@ public class WelcomeActivity extends BaseSetupWizardActivity {
     @Override
     public void onNavigateBack() {
         startEmergencyDialer();
+    }
+	
+	@Override
+    public void onNavigateNext() {
+        onNextPressed();
     }
 
     @Override
