9060716b7d28ad5cdbf3c4b412752587d6df5d85
Chris <chris@cypheros.co>
2017-06-07 00:04:51 -0400

    suw: Import common aoscp infrastructure
    Initial bring up of the aoscp setupwizard.
    Dumps all of lineage's common dependencies,
    such as permissions and action name scheming.
    
    Squash of:
    https://gerrit.cypheros.co/#/c/253/
    https://gerrit.cypheros.co/#/c/254/
    https://gerrit.cypheros.co/#/c/255/
    
    Signed-off by: Mati Halperin <matias.halperin@gmail.com>
    Signed-off by: Chris <chris@cypheros.co>
    
    Change-Id: Ib06b33e1466a1556ef95007b0bdb39275d69e322

--- exit_wizard.sh
+++ exit_wizard.sh
@@ -2,12 +2,12 @@
 
 adb root
 wait ${!}
-adb shell pm enable com.aoscp.setupwizard/com.aoscp.setupwizard.SetupWizardExitActivity || true
+adb shell pm enable org.aoscp.setupwizard/org.aoscp.setupwizard.SetupWizardExitActivity || true
 wait ${!}
 adb shell pm enable com.google.android.setupwizard/com.google.android.setupwizard.SetupWizardExitActivity || true
 wait ${!}
 sleep 1
-adb shell am start com.aoscp.setupwizard/com.aoscp.setupwizard.SetupWizardExitActivity || true
+adb shell am start org.aoscp.setupwizard/org.aoscp.setupwizard.SetupWizardExitActivity || true
 wait ${!}
 sleep 1
 adb shell am start com.google.android.setupwizard/com.google.android.setupwizard.SetupWizardExitActivity
