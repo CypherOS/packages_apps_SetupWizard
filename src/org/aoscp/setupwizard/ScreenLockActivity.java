ad8c1841841808723419b3de3bd0ed257c40166c
Chris <chris@cypheros.co>
2017-06-11 00:34:18 -0400

    suw: New navbar integration policy
    Creates a fork of the defualt navbar button items
    in setupwizard. Follows the new Google implementation,
    by providing a black bar with the system navbar back
    icon to navigate to previous pages. To move forward
    pages, each page will implement a dedicated "Next"
    button.
    
    Change-Id: I0a09f2df77c44d6aff219cf10ccb7a6e26b08472

--- src/org/aoscp/setupwizard/ScreenLockActivity.java
+++ src/org/aoscp/setupwizard/ScreenLockActivity.java
@@ -31,7 +31,6 @@ public class ScreenLockActivity extends SubBaseActivity {
 
     @Override
     protected void onStartSubactivity() {
-        setNextAllowed(true);
         findViewById(R.id.setup_lockscreen).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
