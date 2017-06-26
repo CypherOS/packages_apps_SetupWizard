35341bb5baaaec9bea26132495a1f6c64c390319
Mati Halperin <matias.halperin@gmail.com>
2017-06-26 11:12:27 -0300

    Cleanups and improvements
    Also fix a build error caused by last commit
    
    Change-Id: Ie7e229d81a1b93118a01033fe9d0d9fdbf26ff23

--- src/org/aoscp/setupwizard/util/SetupWizardUtils.java
+++ src/org/aoscp/setupwizard/util/SetupWizardUtils.java
@@ -205,6 +205,18 @@ public class SetupWizardUtils {
         return fingerprintManager.isHardwareDetected();
     }
 
+    public static boolean isAppInstalled(final Context context, final String packageName) {
+        if (uid == -1) {
+            PackageManager pm = context.getPackageManager();
+            try {
+                uid = pm.getPackageUidAsUser(packageName, 0, UserHandle.USER_SYSTEM);
+            } catch (PackageManager.NameNotFoundException e) {
+                uid = -2;
+            }
+        }
+        return uid > -1;
+    }
+
     public static void disableComponentsForMissingFeatures(Context context) {
         disableComponent(context, BluetoothSetupActivity.class);
         if (!hasFingerprint(context)) {
@@ -224,7 +236,7 @@ public class SetupWizardUtils {
         // Googles ATV SUW crashes before finishing, leaving devices
         // unprovisioned. Disable it for now.
         if (hasLeanback(context) &&
-            PackageManagerUtils.isAppInstalled(context, GMS_TV_SUW_PACKAGE)) {
+            isAppInstalled(context, GMS_TV_SUW_PACKAGE)) {
             disableApplication(context, GMS_TV_SUW_PACKAGE);
         }
     }
